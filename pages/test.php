cd53caad6c1a317c1298e3f55d4cc99812926684
unknown <andreifcoelho@gmail.com>
2022-08-09 23:12:24 -0300

    novo layout de email mkt

--- pages/test.php
+++ pages/test.php
@@ -4,7 +4,9 @@
 echo $content;
 */
 
-$content = file_get_contents("../view/email_mkt/pattern.html");
+$content = file_get_contents("../view/email_mkt/email_mkt_plano_de_lutas.html");
 $content = str_replace("{{url}}", url("", true), $content);
+//$content = str_replace("{{url}}", url(), $content);
+echo $content;
 
-file_put_contents("../view/email_mkt_final/pattern.html", $content);
+file_put_contents("../view/email_mkt_final/email_mkt_plano_de_lutas.html", $content);
