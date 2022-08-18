280b831bca663e6c08ce12af090ba039f211af53
unknown <andreifcoelho@gmail.com>
2022-08-18 18:56:58 -0300

    novo email mkt layout

--- pages/test.php
+++ pages/test.php
@@ -3,10 +3,10 @@
 echo $content;
 */
 
-$nome = "email_mkt_reprovacao_acordo.html";
+$nome = "email_mkt_nova_reuniao.html";
 $content = file_get_contents("../view/email_mkt/".$nome);
-$content = str_replace("{{url}}", url("", true), $content);
-//$content = str_replace("{{url}}", url(), $content);
+//$content = str_replace("{{url}}", url("", true), $content);
+$content = str_replace("{{url}}", url(), $content);
 echo $content;
 
-file_put_contents("../view/email_mkt_final/".$nome, $content);
+file_put_contents("../view/email_mkt/".$nome, $content);
