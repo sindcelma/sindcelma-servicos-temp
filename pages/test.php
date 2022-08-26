8bb6649f3d38df3f2baeac6da1301859b9cc452e
unknown <andreifcoelho@gmail.com>
2022-08-26 01:52:29 -0300

    template segunda rodada

--- pages/test.php
+++ pages/test.php
@@ -3,10 +3,10 @@
 echo $content;
 */
 
-$nome = "email_mkt_nova_reuniao.html";
+$nome = "email_mkt_segunda_rodada.html";
 $content = file_get_contents("../view/email_mkt/".$nome);
-//$content = str_replace("{{url}}", url("", true), $content);
-$content = str_replace("{{url}}", url(), $content);
+$content = str_replace("{{url}}", url("", true), $content);
+//$content = str_replace("{{url}}", url(), $content);
 echo $content;
 
-file_put_contents("../view/email_mkt/".$nome, $content);
+file_put_contents("../view/email_mkt_final/".$nome, $content);
